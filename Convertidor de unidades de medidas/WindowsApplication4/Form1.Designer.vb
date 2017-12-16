35813cfde5984015837bccebc13f431d47d60ad0
cristianubri27 <34196832+cristianubri27@users.noreply.github.com>
2017-12-15 20:50:35 -0400

    Add files via upload

---
+++ Convertidor de unidades de medidas/WindowsApplication4/Form1.Designer.vb
@@ -0,0 +1,197 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdSalir = New System.Windows.Forms.Button()
+        Me.cmdBorrar = New System.Windows.Forms.Button()
+        Me.cmdConvertir = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.txtBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'cmdSalir
+        '
+        Me.cmdSalir.Location = New System.Drawing.Point(229, 242)
+        Me.cmdSalir.Name = "cmdSalir"
+        Me.cmdSalir.Size = New System.Drawing.Size(75, 23)
+        Me.cmdSalir.TabIndex = 21
+        Me.cmdSalir.Text = "Salir"
+        Me.cmdSalir.UseVisualStyleBackColor = True
+        '
+        'cmdBorrar
+        '
+        Me.cmdBorrar.Location = New System.Drawing.Point(42, 242)
+        Me.cmdBorrar.Name = "cmdBorrar"
+        Me.cmdBorrar.Size = New System.Drawing.Size(75, 23)
+        Me.cmdBorrar.TabIndex = 20
+        Me.cmdBorrar.Text = "Borrar"
+        Me.cmdBorrar.UseVisualStyleBackColor = True
+        '
+        'cmdConvertir
+        '
+        Me.cmdConvertir.Location = New System.Drawing.Point(122, 202)
+        Me.cmdConvertir.Name = "cmdConvertir"
+        Me.cmdConvertir.Size = New System.Drawing.Size(144, 23)
+        Me.cmdConvertir.TabIndex = 19
+        Me.cmdConvertir.Text = "Convertir"
+        Me.cmdConvertir.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(168, 140)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 13)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "Resultado"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(206, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 13)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "A:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 107)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 13)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "De:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(152, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
+        Me.Label1.TabIndex = 15
+        Me.Label1.Text = "Ingrese un Valor"
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(122, 156)
+        Me.txtResultado.Multiline = True
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.ReadOnly = True
+        Me.txtResultado.Size = New System.Drawing.Size(144, 40)
+        Me.txtResultado.TabIndex = 14
+        Me.txtResultado.TabStop = False
+        Me.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Kilometros", "Metros", "Centimetros", "Milimetros", "Pies", "Pulgadas"})
+        Me.ComboBox2.Location = New System.Drawing.Point(229, 104)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(144, 21)
+        Me.ComboBox2.TabIndex = 13
+        '
+        'txtBox1
+        '
+        Me.txtBox1.Location = New System.Drawing.Point(122, 26)
+        Me.txtBox1.Name = "txtBox1"
+        Me.txtBox1.Size = New System.Drawing.Size(144, 20)
+        Me.txtBox1.TabIndex = 12
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Kilometros", "Metros", "Centimetros", "Milimetros", "Pies", "Pulgadas"})
+        Me.ComboBox1.Location = New System.Drawing.Point(42, 104)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(144, 21)
+        Me.ComboBox1.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(394, 321)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 13)
+        Me.Label5.TabIndex = 22
+        Me.Label5.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(292, 321)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(81, 13)
+        Me.Label6.TabIndex = 23
+        Me.Label6.Text = "Convercion No."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(435, 343)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cmdSalir)
+        Me.Controls.Add(Me.cmdBorrar)
+        Me.Controls.Add(Me.cmdConvertir)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.txtBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "Convertidor de unidades"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents cmdSalir As System.Windows.Forms.Button
+    Friend WithEvents cmdBorrar As System.Windows.Forms.Button
+    Friend WithEvents cmdConvertir As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtResultado As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents txtBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+End Class
---
+++ Convertidor de unidades de medidas/WindowsApplication4/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+    // cristian ubri mañon  15-EISM-1-024 //
+    Private Sub cmdConvertir_Click(sender As Object, e As EventArgs) Handles cmdConvertir.Click
+        Dim N1 As Double
+        Dim U As String
+
+        U = ""
+        If ComboBox1.Text = "Milimetros" And ComboBox2.Text = "Milimetros" Then
+            N1 = Val(txtBox1.Text)
+            U = "mm"
+        ElseIf ComboBox1.Text = "Milimetros" And ComboBox2.Text = "Centimetros" Then
+            N1 = Val(txtBox1.Text) / 10
+            U = "cm"
+        ElseIf ComboBox1.Text = "Milimetros" And ComboBox2.Text = "Metros" Then
+            N1 = Val(txtBox1.Text) / 1000
+            U = "m"
+        ElseIf ComboBox1.Text = "Milimetros" And ComboBox2.Text = "Kilometros" Then
+            N1 = Val(txtBox1.Text) / 1000000
+            U = "km"
+        ElseIf ComboBox1.Text = "Centimetros" And ComboBox2.Text = "Milimetros" Then
+            N1 = Val(txtBox1.Text) * 10
+            U = "mm"
+        ElseIf ComboBox1.Text = "Centimetros" And ComboBox2.Text = "Centimetros" Then
+            N1 = Val(txtBox1.Text)
+            U = "cm"
+        ElseIf ComboBox1.Text = "Centimetros" And ComboBox2.Text = "Metros" Then
+            N1 = Val(txtBox1.Text) / 100
+            U = "m"
+        ElseIf ComboBox1.Text = "Centimetros" And ComboBox2.Text = "Kilometros" Then
+            N1 = Val(txtBox1.Text) / 100000
+            U = "km"
+        ElseIf ComboBox1.Text = "Metros" And ComboBox2.Text = "Milimetros" Then
+            N1 = Val(txtBox1.Text) * 1000
+            U = "mm"
+        ElseIf ComboBox1.Text = "Metros" And ComboBox2.Text = "Centimetros" Then
+            N1 = Val(txtBox1.Text) * 100
+            U = "cm"
+        ElseIf ComboBox1.Text = "Metros" And ComboBox2.Text = "Metros" Then
+            N1 = Val(txtBox1.Text)
+            U = "m"
+        ElseIf ComboBox1.Text = "Metros" And ComboBox2.Text = "Kilometros" Then
+            N1 = Val(txtBox1.Text) / 1000
+            U = "km"
+        ElseIf ComboBox1.Text = "Kilometros" And ComboBox2.Text = "Milimetros" Then
+            N1 = Val(txtBox1.Text) * 1000000
+            U = "mm"
+        ElseIf ComboBox1.Text = "Kilometros" And ComboBox2.Text = "Centimetros" Then
+            N1 = Val(txtBox1.Text) * 100000
+            U = "cm"
+        ElseIf ComboBox1.Text = "Kilometros" And ComboBox2.Text = "Metros" Then
+            N1 = Val(txtBox1.Text) * 1000
+            U = "m"
+        ElseIf ComboBox1.Text = "Pies" And ComboBox2.Text = "Pulgadas" Then
+            N1 = Val(txtBox1.Text) * 12
+            U = "Pul"
+        ElseIf ComboBox1.Text = "Pulgadas" And ComboBox2.Text = "Pies" Then
+            N1 = Val(txtBox1.Text) / 12
+            U = "Pies"
+
+        End If
+        txtResultado.Text = N1 & U
+    End Sub
+
+    Private Sub cmdSalir_Click(sender As Object, e As EventArgs) Handles cmdSalir.Click
+        Dim M As String
+
+        M = MsgBox("Seguro que desea salir?", vbYesNo, "Salir")
+
+        If M = vbYes Then
+            End
+        Else
+
+        End If
+
+
+    End Sub
+
+    Private Sub cmdBorrar_Click(sender As Object, e As EventArgs) Handles cmdBorrar.Click
+        txtBox1.Text = ""
+        txtResultado.Text = ""
+        ComboBox1.Text = ""
+        ComboBox2.Text = ""
+    End Sub
+
+   
+
+    Private Sub txtBox1_TextChanged(sender As Object, e As EventArgs) Handles txtBox1.TextChanged
+
+    End Sub
+
+    Private Sub Cambiodeunidades_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
